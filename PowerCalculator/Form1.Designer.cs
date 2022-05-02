6aa4c5b5cbfa65cd326b6124f4293f8089435073
Abdul Hameed <ahameed.bcss19@iba-suk.edu.pk>
2022-05-02 13:20:44 +0500

    first commit

---
+++ PowerCalculator/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace PowerCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tbDevice = new System.Windows.Forms.TextBox();
+            this.tbPower = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbAverage = new System.Windows.Forms.TextBox();
+            this.tbSum = new System.Windows.Forms.TextBox();
+            this.devicesDataSet3 = new PowerCalculator.DevicesDataSet3();
+            this.devicesTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.devicesTableTableAdapter = new PowerCalculator.DevicesDataSet3TableAdapters.DevicesTableTableAdapter();
+            this.deviceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.powerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.devicesDataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.devicesTableBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.deviceDataGridViewTextBoxColumn,
+            this.powerDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.devicesTableBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(243, 351);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(291, 230);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(62, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(371, 230);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(59, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // tbDevice
+            // 
+            this.tbDevice.Location = new System.Drawing.Point(287, 132);
+            this.tbDevice.Name = "tbDevice";
+            this.tbDevice.Size = new System.Drawing.Size(143, 20);
+            this.tbDevice.TabIndex = 3;
+            // 
+            // tbPower
+            // 
+            this.tbPower.Location = new System.Drawing.Point(291, 182);
+            this.tbPower.Name = "tbPower";
+            this.tbPower.Size = new System.Drawing.Size(143, 20);
+            this.tbPower.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Device";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Power";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 413);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sum of Power:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 374);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Average:";
+            // 
+            // tbAverage
+            // 
+            this.tbAverage.Enabled = false;
+            this.tbAverage.Location = new System.Drawing.Point(124, 374);
+            this.tbAverage.Name = "tbAverage";
+            this.tbAverage.Size = new System.Drawing.Size(143, 20);
+            this.tbAverage.TabIndex = 9;
+            // 
+            // tbSum
+            // 
+            this.tbSum.Enabled = false;
+            this.tbSum.Location = new System.Drawing.Point(124, 413);
+            this.tbSum.Name = "tbSum";
+            this.tbSum.Size = new System.Drawing.Size(143, 20);
+            this.tbSum.TabIndex = 10;
+            // 
+            // devicesDataSet3
+            // 
+            this.devicesDataSet3.DataSetName = "DevicesDataSet3";
+            this.devicesDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // devicesTableBindingSource
+            // 
+            this.devicesTableBindingSource.DataMember = "DevicesTable";
+            this.devicesTableBindingSource.DataSource = this.devicesDataSet3;
+            // 
+            // devicesTableTableAdapter
+            // 
+            this.devicesTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // deviceDataGridViewTextBoxColumn
+            // 
+            this.deviceDataGridViewTextBoxColumn.DataPropertyName = "Device";
+            this.deviceDataGridViewTextBoxColumn.HeaderText = "Device";
+            this.deviceDataGridViewTextBoxColumn.Name = "deviceDataGridViewTextBoxColumn";
+            // 
+            // powerDataGridViewTextBoxColumn
+            // 
+            this.powerDataGridViewTextBoxColumn.DataPropertyName = "Power";
+            this.powerDataGridViewTextBoxColumn.HeaderText = "Power";
+            this.powerDataGridViewTextBoxColumn.Name = "powerDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 450);
+            this.Controls.Add(this.tbSum);
+            this.Controls.Add(this.tbAverage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPower);
+            this.Controls.Add(this.tbDevice);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Power Consumption";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.devicesDataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.devicesTableBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox tbDevice;
+        private System.Windows.Forms.TextBox tbPower;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbAverage;
+        private System.Windows.Forms.TextBox tbSum;
+        private DevicesDataSet3 devicesDataSet3;
+        private System.Windows.Forms.BindingSource devicesTableBindingSource;
+        private DevicesDataSet3TableAdapters.DevicesTableTableAdapter devicesTableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deviceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn powerDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ PowerCalculator/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.devicesTableTableAdapter.Fill(this.devicesDataSet3.DevicesTable);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'devicesDataSet3.DevicesTable' table. You can move, or remove it, as needed.
+            //  this.devicesTableTableAdapter.Fill(this.devicesDataSet3.DevicesTable);
+            Sum();
+            Average();
+
+        }
+
+        public void Average()
+        {
+            int Sum = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                Sum += Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+            Sum = Sum / (dataGridView1.Rows.Count-1);
+            tbAverage.Text = Sum + "";
+        }
+
+        public void Sum()
+        {
+            /*  tbSum.Text = (from DataGridViewRow row in dataGridView1.Rows
+                                 where row.Cells[1].FormattedValue.ToString() != string.Empty
+                                 select Convert.ToInt32(row.Cells[1].FormattedValue)).Sum().ToString();
+          */
+            int Sum = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                Sum += Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+            tbSum.Text = Sum+"";
+         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+            SqlConnection connection;
+            connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source = (localdb)\MSSQLLocalDB;
+                                            Initial Catalog=Devices;
+                                            Integrated Security=true";
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+
+                String device = tbDevice.Text;
+                int power = Convert.ToInt32(tbPower.Text);
+                command.CommandText = "Insert into DevicesTable(Device,Power) values('" + device + "','" + power + "');";
+
+            
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            devicesTableTableAdapter.Fill(devicesDataSet3.DevicesTable);
+            Sum();
+            Average();
+
+        }
+
+       
+      
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection;
+            connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source = (localdb)\MSSQLLocalDB;
+                                            Initial Catalog=Devices;
+                                            Integrated Security=true";
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            
+            String device = tbDevice.Text;
+            int power = Convert.ToInt32(tbPower.Text);
+            command.CommandText = "Delete From DevicesTable Where Device='"+device+"' AND Power='"+power+"';";
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            devicesTableTableAdapter.Fill(devicesDataSet3.DevicesTable);
+            Sum();
+            Average();
+        }
+    }
+}
